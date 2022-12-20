4a786b07d236cc677a66f4995e172d9d72ee7079
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-12-20 16:02:51 -0300

    Implementando teste unitário para a view excluir fluxo quando há etapas cadastradas #66

--- flows/tests/flows/views/fluxo/test_excluir_fluxo_view.py
+++ flows/tests/flows/views/fluxo/test_excluir_fluxo_view.py
@@ -1,12 +1,13 @@
 from django.test import Client, TestCase
 from django.urls import reverse_lazy, resolve
-from flows.models import Fluxo
+from flows.models import Fluxo, Etapa
 
 class ExcluirFluxoView(TestCase):
     def setUp(self):
         self.client = Client()
         self.fluxo_obj = Fluxo.objects.create(nome="Fluxo teste", descricao="Descrição teste")
         self.targetUrl = reverse_lazy('flows:excluir_fluxo', kwargs={'pk': self.fluxo_obj.id})
+        
     
     def test_excluir_fluxo(self):
         """Verifica se o fluxo é excluído, caso não haja etapas cadastradas no fluxo"""
@@ -23,8 +24,24 @@ class ExcluirFluxoView(TestCase):
         self.assertEqual(quant_flows, 1)
 
     def test_the_context_of_the_response(self):
-        """Verifica se existem fluxos no contexto da respose"""
+        """Verifica se existem fluxos no contexto da response"""
 
         response = self.client.delete(self.targetUrl)
         self.assertTrue('flow' in response.context)
 
+    def test_the_deletion_of_the_flow_with_registered_steps(self):
+        """Verifica como o sistema se comporta quando existem etapas vinculadas ao fluxo"""
+
+        self.etapa_iniciada = Etapa.objects.create(
+            nome="Etapa teste",
+            descricao="Descrição teste",
+            numero='1',
+            data_inicio='2022-12-01',
+            data_finalizacao='2022-12-31',
+            gameficada=True,
+            fluxo = self.fluxo_obj
+        )
+
+        self.client.post(self.targetUrl)
+        quant_flows = Fluxo.objects.count()
+        self.assertEqual(quant_flows, 1)
