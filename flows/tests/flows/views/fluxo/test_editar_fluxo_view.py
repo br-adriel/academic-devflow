163c19ca0715e722b3b81e3dc6d0e2c151cddaec
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-12-20 11:14:19 -0300

    implementando teste unitário para o caso de enviar uma instância incorreta para a view editar fluxo #66

--- flows/tests/flows/views/fluxo/test_editar_fluxo_view.py
+++ flows/tests/flows/views/fluxo/test_editar_fluxo_view.py
@@ -30,7 +30,7 @@ class EditarFluxoView(TestCase):
         self.assertEqual(found.func, editar_fluxo_view)
 
     def test_editing_for_a_non_existing_object(self):
-        """Verifica """
+        """Verifica como o sistema se comporta quando a url contém um id não existente"""
         url = reverse_lazy('flows:editar_fluxo', kwargs={'pk': 100})
 
         data = {
@@ -39,5 +39,18 @@ class EditarFluxoView(TestCase):
         }
 
         response = self.client.post(url, data)
-
         self.assertEqual(response.status_code, 404)
+
+    def test_editing_by_sending_invalid_data(self):
+        """Verifica como o sistema se comporta quando é enviado uma instância incorreta"""
+        data = {
+            'name': "Fluxo errado",
+            'description': "Descrição errada"
+        }
+
+        self.client.post(self.targetUrl, data)
+        fluxo_atualizado = Fluxo.objects.get(id=self.fluxo_obj.id)
+        self.assertNotEqual(fluxo_atualizado.nome, 'Fluxo errado')
+        self.assertNotEqual(fluxo_atualizado.nome, 'Descrição errada')
+        
+
