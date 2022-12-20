815d29f33da8b38826a12d252ce1cf45cdba0c6c
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-12-19 23:17:59 -0300

    corrigindo a url da view editar fluxo e inserido a url no âncora #63

--- flows/urls.py
+++ flows/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', pagina_inicial_view, name='inicio'),
     path('adicionar', adicionar_fluxo_view, name='adicionar_fluxo'),
     path('<int:pk>', detalhes_fluxo_view, name='detalhes_fluxo'),
-    path('<int:pk>', editar_fluxo_view, name='editar_fluxo'),
+    path('<int:pk>/editar', editar_fluxo_view, name='editar_fluxo'),
     path('<int:fluxo_pk>/adicionar', adicionar_etapa_view, name='adicionar_etapa'),
     path('<int:fluxo_pk>/editar/<int:pk>',
          editar_etapa_view, name='editar_etapa'),
