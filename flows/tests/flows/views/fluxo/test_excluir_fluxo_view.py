a1168992a3d504090e968f4e30a5aedb8842012a
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-12-20 14:36:31 -0300

    implementando teste unitário para a view excluir fluxo quando não há etapas vinculadas #66

--- flows/tests/flows/views/fluxo/test_excluir_fluxo_view.py
+++ flows/tests/flows/views/fluxo/test_excluir_fluxo_view.py
@@ -8,3 +8,9 @@ class ExcluirFluxoView(TestCase):
         self.fluxo_obj = Fluxo.objects.create(nome="Fluxo teste", descricao="Descrição teste")
         self.targetUrl = reverse_lazy('flows:excluir_fluxo', kwargs={'pk': self.fluxo_obj.id})
     
+    def test_excluir_fluxo(self):
+        """Verifica se o fluxo é excluído, caso não haja etapas cadastradas no fluxo"""
+
+        self.client.post(self.targetUrl)
+        quant_flows = Fluxo.objects.count()
+        self.assertEqual(quant_flows, 0)
